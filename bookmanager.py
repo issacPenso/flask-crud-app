d046f440c5f32d742f1d22d73931989447ea04dd
izikp <izikp@radware.com>
2018-03-14 17:30:40 +0200

    display imgaes now

--- bookmanager.py
+++ bookmanager.py
@@ -1,62 +1,92 @@
+import os
+from io import BytesIO
+
 from flask import Flask
 from flask import render_template
 from flask import request
 from flask import redirect
+from flask import send_file
+from flask import url_for
+from sqlalchemy_imageattach.context import store_context
+# from werkzeug.utils import secure_filename
 
 from flask_sqlalchemy import SQLAlchemy
 
-database_file = 'postgres://drzkyzgd:UbG7pFA7U7wdVQ1B4P5opG8l6J5wU3RH@elmer.db.elephantsql.com:5432/drzkyzgd'
+UPLOAD_FOLDER = '/path/to/the/uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+# database_file = 'postgres://drzkyzgd:UbG7pFA7U7wdVQ1B4P5opG8l6J5wU3RH@elmer.db.elephantsql.com:5432/drzkyzgd'
+
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = 'sqlite:///{}'.format(os.path.join(project_dir, 'bookdatabase.db'))
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+app.config['SQLALCHEMY_DATABASE_URI'] = database_file
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 db = SQLAlchemy(app)
 
 
-class Book(db.Model):
+class Books(db.Model):
     title = db.Column(db.String(80), unique=True, nullable=False, primary_key=True)
+    img = db.Column(db.LargeBinary, unique=False, nullable=True, primary_key=False)
 
     def __repr__(self):
-        return "<Title: {}>".format(self.title)
+        return '<Title: {}>'.format(self.title)
 
 
-@app.route('/', methods=["GET", "POST"])
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
     books = None
     if request.form:
         try:
-            book = Book(title=request.form.get("title"))
+            img = request.files['img']
+            book = Books(title=request.form.get('title'), img=img.read())
             db.session.add(book)
             db.session.commit()
         except Exception as e:
-            print("Failed to add book")
+            print('Failed to add book')
             print(e)
-    books = Book.query.all()
-    return render_template("home.html", books=books)
+    books = Books.query.all()
+    return render_template('home.html', books=books)
 
 
-@app.route("/update", methods=["POST"])
+@app.route('/update', methods=['POST'])
 def update():
     try:
-        newtitle = request.form.get("newtitle")
-        oldtitle = request.form.get("oldtitle")
-        book = Book.query.filter_by(title=oldtitle).first()
+        newtitle = request.form.get('newtitle')
+        oldtitle = request.form.get('oldtitle')
+        book = Books.query.filter_by(title=oldtitle).first()
+        img = request.files['img']
+        book.img = img.read()
         book.title = newtitle
         db.session.commit()
     except Exception as e:
-        print("Couldn't update book title")
+        print('Couldn\'t update book title')
         print(e)
-    return redirect("/")
+    return redirect('/')
 
 
-@app.route("/delete", methods=["POST"])
+@app.route('/delete', methods=['POST'])
 def delete():
-    title = request.form.get("title")
-    book = Book.query.filter_by(title=title).first()
+    title = request.form.get('title')
+    book = Books.query.filter_by(title=title).first()
     db.session.delete(book)
     db.session.commit()
-    return redirect("/")
+    return redirect('/')
 
 
-if __name__ == "__main__":
+@app.route('/images/<book_title>')
+def get_book_image_url(book_title):
+    book = Books.query.filter_by(title=book_title).first()
+    return send_file(BytesIO(book.img), attachment_filename=book.title, as_attachment=False)
+
+
+if __name__ == '__main__':
     app.run(debug=True)
